82cb283d77a4028108f302dd5bc838b2109d49ec
Fintan Halpenny <rossmanf@tcd.ie>
2015-12-01 22:41:29 +0000

    Finished list exercises

--- Chapter3/List.scala
+++ Chapter3/List.scala
@@ -4,6 +4,35 @@ case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
+    def append[A](l1: List[A], l2: List[A]): List[A] = l1 match {
+        case Nil => l2
+        case Cons(a, as) => Cons(a, append(as, l2))
+    }
+
+    def tail[A](as: List[A]): List[A] =
+        drop(as, 1)
+
+    def init[A](l: List[A]): List[A] = l match {
+        case Nil          => Nil
+        case Cons(a, Nil) => Nil
+        case Cons(a, as)  => Cons(a, init(as))
+    }
+
+    def setHead[A](as: List[A], v: A): List[A] = as match {
+        case Nil => Nil
+        case Cons(x, xs) => Cons(v, xs)
+    }
+
+    def drop[A](l: List[A], n: Int): List[A] = (l, n) match {
+        case (Nil, _) => Nil
+        case (Cons(a, as), n) => drop(as, n-1)
+    }
+
+    def dropWhile[A](l: List[A], p: A => Boolean): List[A] = l match {
+        case Nil => Nil
+        case Cons(a, as) => if (p(a)) dropWhile(as, p) else Cons(a, as)
+    }
+
     def sum(ints: List[Int]): Int = ints match {
         case Nil => 0
         case Cons(x, xs) => x + sum(xs)
