65875cdf264cf0436b865ca5eebb5f4a09a79bca
Fintan Halpenny <rossmanf@tcd.ie>
2015-12-02 16:31:53 +0000

    Finished list exercises apart from hasSubsequence

--- Chapter3/List.scala
+++ Chapter3/List.scala
@@ -48,6 +48,46 @@ object List {
         go(as, z, f)
     }
 
+    def map[A, B](as: List[A])(f: A => B): List[B] =
+        foldRight(as, Nil: List[B])((a, b) => Cons(f(a), b))
+
+    def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] =
+        foldLeft(map(as)(f), Nil: List[B])(append(_, _))
+
+    def zipWith[A, B, C](as: List[A], bs: List[B])(f: (A, B) => C): List[C] = {
+        @annotation.tailrec
+        def go[A, B, C](as: List[A], bs: List[B], cs: List[C])(f: (A, B) => C): List[C] =
+            (as, bs) match {
+                case (Nil, Nil) => cs
+                case (Cons(x, xs), Cons(y, ys)) => go(xs, ys, Cons(f(x, y), cs))(f)
+            }
+
+        reverse(go(as, bs, Nil)(f))
+    }
+
+    // TODO: Not correct implementation need to revisit
+    // def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = {
+    //     def checkSuper(supI: List[A], sub: List[A]): Boolean =
+    //         (supI, sub) match {
+    //             case (Nil, Nil) => true
+    //             case (Cons(x, xs), Cons(y, ys)) => if (x == y) checkSuper(xs, ys)
+    //                                                else false
+    //             case _ => false
+    //         }
+    //
+    //     sup match {
+    //         case Nil => false
+    //         case Cons(x, xs) => if (checkSuper(Cons(x, xs), sub)) true
+    //                             else hasSubsequence(xs, sub)
+    //     }
+    // }
+
+    def filter[A](as: List[A])(p: A => Boolean): List[A] =
+        foldRight(as, Nil: List[A])((a, b) => if (p(a)) Cons(a, b) else b)
+
+    def filter2[A](as: List[A])(p: A => Boolean): List[A] =
+        flatMap(as)(a => if (p(a)) List(a) else Nil)
+
     def length[A](as: List[A]): Int =
         foldLeft(as, 0)((b, a) => 1 + b)
 
