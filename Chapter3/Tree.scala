3b91814274d2dc535699b137bba94630ccfbade9
Fintan Halpenny <rossmanf@tcd.ie>
2015-12-02 16:58:54 +0000

    Added Tree code - depth not working right

---
+++ Chapter3/Tree.scala
@@ -0,0 +1,26 @@
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Tree {
+    def size[A](t: Tree[A]): Int = t match {
+        case Leaf(_) => 1
+        case Branch(l: Tree[A], r: Tree[A]) => 1 + size(l) + size(r)
+    }
+
+    def maximum[A](t: Tree[Int]): Int = t match {
+        case Leaf(v) => v
+        case Branch(l: Tree[Int], r: Tree[Int]) => maximum(l) max maximum(r)
+    }
+
+    // TODO: depth is wrong
+    def depth[A](t: Tree[A]): Int = t match {
+        case Leaf(_) => 1
+        case Branch(l: Tree[A], r: Tree[A]) => (size(l) - 1) max (size(r) - 1)
+    }
+
+    def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+        case Leaf(v) => Leaf(f(v))
+        case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+    }
+}
